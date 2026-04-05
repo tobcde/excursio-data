0222a49bb01cf5baf6b89bdbf2d31392bff65483
tobcde <magalottipablo@gmail.com>
2026-04-05 20:40:08 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,42 @@
+const CACHE_NAME = 'excursio-data-v2';
+const STATIC_FILES = [
+  '/excursio-data/',
+  '/excursio-data/index.html',
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(STATIC_FILES).catch(err => console.log('Cache parcial:', err));
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  if (event.request.url.includes('script.google.com') ||
+      event.request.url.includes('googleusercontent.com') ||
+      event.request.url.includes('generativelanguage.googleapis.com')) {
+    return;
+  }
+  event.respondWith(
+    fetch(event.request)
+      .then(response => {
+        if (response.ok && event.request.method === 'GET') {
+          const clone = response.clone();
+          caches.open(CACHE_NAME).then(cache => cache.put(event.request, clone));
+        }
+        return response;
+      })
+      .catch(() => caches.match(event.request))
+  );
+});
